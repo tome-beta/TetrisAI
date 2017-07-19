0378f481d1d5d5ca39138f10f86ef521ceb8f7bd
Susumu-Fukutome <tomex.beta@gmail.com>
2017-07-20 00:19:01 +0900

    #1 とりあえず種類を選んでブロックを書けるようになった

--- tetris/GameField.cs
+++ tetris/GameField.cs
@@ -15,10 +15,14 @@ namespace tetris
         public GameField()
         {
             InitializeComponent();
+
+            Init();
             DispTest();
         }
 
-
+        private void Init()
+        {
+        }
         private void DispTest()
         {
             //描画先とするImageオブジェクトを作成する
@@ -26,20 +30,17 @@ namespace tetris
             //ImageオブジェクトのGraphicsオブジェクトを作成する
             Graphics g = Graphics.FromImage(canvas);
 
-            //画像ファイルを読み込んで、Imageオブジェクトとして取得する
-            Image img = Image.FromFile(@"D:\myprog\tetris_work\issue1\TetrisAI\tetris\resouce\mino.bmp");
-            //画像をcanvasの座標(20, 10)の位置に描画する
-            g.DrawImage(img, 20, 10, img.Width, img.Height);
-            //Imageオブジェクトのリソースを解放する
-            img.Dispose();
 
-            //Graphicsオブジェクトのリソースを解放する
+            BlockInfo I_mino = blockControle.GetBlock(BlockInfo.BlockType.MINO_J);
+            I_mino.Draw(g, blockControle.BlockSourceImage);
+
+            //Imageオブジェクトのリソースを解放する
+           //Graphicsオブジェクトのリソースを解放する
             g.Dispose();
             //PictureBox1に表示する
             this.pictureBoxField1P.Image = canvas;
         }
 
-        BlockInfo tmp_block = new BlockInfo();
-
+        BlockControle blockControle = new BlockControle();
     }
 }
