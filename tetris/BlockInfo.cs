00f7cd9de9e1fe4b1b51dbc247360d4b61cda572
Susumu-Fukutome <tomex.beta@gmail.com>
2017-07-23 22:13:35 +0900

    #5　ミノの形で画像を表示するようにした

--- tetris/BlockInfo.cs
+++ tetris/BlockInfo.cs
@@ -22,14 +22,83 @@ namespace tetris
             MINO_O,
         };
 
+        private int[,] BlockShape = new int[,]
+        {
+            //ATTACK
+            {
+                1,0,0,0,
+                0,0,0,0,
+                0,0,0,0,
+                0,0,0,0,
+            },
+            //I
+            {
+                0,1,0,0,
+                0,1,0,0,
+                0,1,0,0,
+                0,1,0,0,
+            },
+            //T
+            {
+                0,1,0,0,
+                1,1,1,0,
+                0,0,0,0,
+                0,0,0,0,
+            },
+            //J
+            {
+                1,0,0,0,
+                1,1,1,0,
+                0,0,0,0,
+                0,0,0,0,
+            },
+            //L
+            {
+                0,0,1,0,
+                1,1,1,0,
+                0,0,0,0,
+                0,0,0,0,
+            },
+            //Z
+            {
+                1,1,0,0,
+                0,1,1,0,
+                0,0,0,0,
+                0,0,0,0,
+            },
+            //S
+            {
+                0,1,1,0,
+                1,1,0,0,
+                0,0,0,0,
+                0,0,0,0,
+            },
+            //O
+            {
+                0,1,1,0,
+                0,1,1,0,
+                0,0,0,0,
+                0,0,0,0,
+            },
+        };
+
+
         public BlockInfo()
         {
-
+            //使わない
         }
 
         public BlockInfo(BlockType type)
         {
             this.type = type;
+
+            shape = new int[16];
+
+            for(int i = 0; i < 16; i++)
+            {
+                shape[i] = BlockShape[(int)type, i];
+            }
+
         }
 
         //ミノを描く
@@ -37,15 +106,25 @@ namespace tetris
         {
             //ミノの種類により切り出す画像を選ぶ
             int y_pos = (int)(this.type) * BLOCK_HEIGHT;
-
-
             Rectangle srcRect = new Rectangle(0, y_pos, BLOCK_WIDTH, BLOCK_HEIGHT);
             Rectangle desRect = new Rectangle(0, 0, srcRect.Width, srcRect.Height);
-            g.DrawImage(source_image, desRect, srcRect, GraphicsUnit.Pixel);
+
+            for(int i = 0; i < 16;i++)
+            {
+                if( shape[i] != 0)
+                {
+                    int x = i % 4;
+                    int y = i / 4;
+                    desRect.X = x * BLOCK_WIDTH;
+                    desRect.Y = y * BLOCK_HEIGHT;
+                    g.DrawImage(source_image, desRect, srcRect, GraphicsUnit.Pixel);
+                }
+            }
 
         }
 
         public BlockType type;
+        private int[] shape;
 
         const int BLOCK_WIDTH = 30;
         const int BLOCK_HEIGHT = 30;
