4f68c83a6552bd3464956bfe8b8d5a6a60997618
Susumu-Fukutome <tomex.beta@gmail.com>
2017-07-31 10:01:31 +0900

    #14 メインループの作成

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -8,15 +8,43 @@ namespace tetris
 {
     static class Program
     {
+        //60FPSでの処理
+        private const int waitTimes = (int)(1000.0 / 60.0);
+        private const int FPS = 60;
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GameField());
+            GameField field = new GameField();
+            field.Show();
+
+            int frame = 0;
+            int before = Environment.TickCount;
+
+
+            int targetTimes = System.Environment.TickCount & int.MaxValue;
+            targetTimes += waitTimes;
+
+            while(field.Created)
+            {
+                int now = Environment.TickCount;
+                int progress = now - before;
+                int ideal = (int)(frame * (1000.0F / FPS));
+
+                if (ideal > progress) System.Threading.Thread.Sleep(ideal - progress);
+
+                frame++;
+                if (progress >= 1000)
+                {
+                    //メインの処理
+                    field.MainLoop(frame);
+                    before = now;
+                    frame = 0;
+                }
+                Application.DoEvents();             //windouwsメッセージ処理
+            }
         }
     }
 }
