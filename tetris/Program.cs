2f57654454563d6a8b2a26238369db1992449d09
Susumu-Fukutome <tomex.beta@gmail.com>
2017-08-12 13:43:53 +0900

    #40 FPSの処理を作り直し

--- tetris/Program.cs
+++ tetris/Program.cs
@@ -22,19 +22,58 @@ namespace tetris
             GameField field = new GameField();
             field.Show();
 
-            double targetTime;
-            targetTime = (double)System.Environment.TickCount;
-            targetTime += waitTime;
+            // 次に処理するフレームの時刻（初回は即処理するので初期値は現在時刻をセット）
+            double nextFrame = (double)System.Environment.TickCount;
+            // フレームを処理する周期（1/60秒）
+            float period = 1000f / 60f;
+
+            double lastTime = (double)System.Environment.TickCount;
+            int fps = 60;
+            double fpsLimiter = 0.0f;
+            //ゲームループ
             while (field.Created)
             {
-                if ((double)System.Environment.TickCount >= targetTime)
+                //開始時間を取得
+                double startTime = (double)System.Environment.TickCount;
+
+                //前回の呼び出しからの経過時間
+                double deltaTime = startTime - lastTime;
+
+///                mainForm.deltaTime = deltaTime;
+
+                //フレームレートの計算
+                fpsLimiter += deltaTime;
+
+                if (fpsLimiter > 1000)
                 {
-                    //メインの処理
-                    field.MainLoop();
-                    targetTime += waitTime;
+                    fpsLimiter -= 1000;
+                    field.fps = fps;
+                    fps = 0;
                 }
-                System.Threading.Thread.Sleep(1);
+
+                // ここで描画以外の計算処理を行う。
+                //メインの処理
+                field.Exec();
+
+                //描画処理
+                field.DrawUpdate();
+
+                fps++;
+
+                lastTime = startTime;
+
+                // Windowsメッセージを処理させる
                 Application.DoEvents();
+
+                //処理全体の時間が最大フレーム時間に満たない場合、スリープする
+                double procTime = (double)System.Environment.TickCount - startTime;
+
+                // 1ms以上の間があるか？
+                if (period - procTime > 1)
+                {
+                    // Sleepする
+                    System.Threading.Thread.Sleep((int)(period - procTime));
+                }
             }
         }
     }
