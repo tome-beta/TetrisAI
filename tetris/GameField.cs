f1a0ac4e351e677225e78daa3026a4e54ac8984e
Susumu-Fukutome <tomex.beta@gmail.com>
2017-08-14 12:28:12 +0900

    #53 ブロック描画関数をまとめた

--- tetris/GameField.cs
+++ tetris/GameField.cs
@@ -182,50 +182,6 @@ namespace tetris
         //  private
         //===========================================
 
-        private void DrawCurrentBlock()
-        {
-            if (this.blockControle.CurrentBlock != null)
-            {
-                //名前おきかえ
-                Point Pos = this.blockControle.CurrentPos;
-                BlockInfo CurrnetInfo = this.blockControle.CurrentBlock;
-
-                //ミノの種類により切り出す画像を選ぶ
-                int y_pos = (int)(CurrnetInfo.type) * BlockInfo.BLOCK_HEIGHT;
-                Rectangle srcRect = new Rectangle(0, y_pos, BlockInfo.BLOCK_WIDTH, BlockInfo.BLOCK_HEIGHT);
-                Rectangle desRect = new Rectangle(0, 0, srcRect.Width, srcRect.Height);
-
-                for (int y = 0; y < BlockInfo.BLOCK_CELL_HEIGHT; y++)
-                {
-                    for (int x = 0; x < BlockInfo.BLOCK_CELL_WIDTH; x++)
-                    {
-                        if (CurrnetInfo.shape[(int)CurrnetInfo.block_rot, y, x] != 0)
-                        {
-                            desRect.X = (Pos.X + x) * BlockInfo.BLOCK_WIDTH;
-                            desRect.Y = (Pos.Y + y) * BlockInfo.BLOCK_HEIGHT;
-                            gFiled1P.DrawImage(BlockSourceImage, desRect, srcRect, GraphicsUnit.Pixel);
-                        }
-                    }
-                }
-
-                //デバッグ用にブロック領域の線を引いておく
-                using (Pen pen = new Pen(Color.Pink))
-                {
-                    for (int x = 0; x < 5; x++)
-                    {
-                        gFiled1P.DrawLine(pen, new Point((Pos.X + x) * BlockInfo.BLOCK_WIDTH, (Pos.Y + 0) * BlockInfo.BLOCK_HEIGHT),
-                            new Point((Pos.X + x) * BlockInfo.BLOCK_WIDTH, (Pos.Y + 4) * BlockInfo.BLOCK_HEIGHT));
-                    }
-                    for (int y = 0; y < 5; y++)
-                    {
-                        gFiled1P.DrawLine(pen, new Point((Pos.X + 0) * BlockInfo.BLOCK_WIDTH, (Pos.Y + y) * BlockInfo.BLOCK_HEIGHT),
-                            new Point((Pos.X + 4) * BlockInfo.BLOCK_WIDTH, (Pos.Y + y) * BlockInfo.BLOCK_HEIGHT));
-                    }
-                }
-
-            }
-        }
-
 
         //NEXTブロックを取得
         private int GetNextBlock()
@@ -236,34 +192,6 @@ namespace tetris
             return type;
         }
 
-        //NEXTブロックの描画
-        private void DrawNextBlock()
-        {
-            //表示位置のクリア
-            gNextBlock1P.Clear(Color.White);
-
-            //ミノの種類により切り出す画像を選ぶ
-            for (int next_num = 0;next_num < NEXT_BLOCK_DISP_NUM; next_num++)
-            {
-                BlockInfo info = new BlockInfo((BlockInfo.BlockType)(NextBlock[next_num]));
-                int y_pos = (int)(NextBlock[next_num]) * BlockInfo.BLOCK_HEIGHT;
-                Rectangle srcRect = new Rectangle(0, y_pos, BlockInfo.BLOCK_WIDTH, BlockInfo.BLOCK_HEIGHT);
-                Rectangle desRect = new Rectangle(0, 0, srcRect.Width, srcRect.Height);
-
-                for (int y = 0; y < BlockInfo.BLOCK_CELL_HEIGHT; y++)
-                {
-                    for (int x = 0; x < BlockInfo.BLOCK_CELL_WIDTH; x++)
-                    {
-                        if (info.shape[(int)info.block_rot, y, x] != 0)
-                        {
-                            desRect.X = (x) * BlockInfo.BLOCK_WIDTH;
-                            desRect.Y = (next_num*3 + y) * BlockInfo.BLOCK_HEIGHT;
-                            gNextBlock1P.DrawImage(BlockSourceImage, desRect, srcRect, GraphicsUnit.Pixel);
-                        }
-                    }
-                }
-            }
-        }
         //NEXTブロックを更新
         private void UpdateNextBlock()
         {
@@ -293,46 +221,6 @@ namespace tetris
             }
         }
 
-        //フィールドに置かれたブロックを描く
-        private void DrawGameField()
-        {
-            //フィールドのクリア
-            gFiled1P.Clear(Color.White);
-
-            //デバッグ用にフィールドに線を引いておく
-            using (Pen pen = new Pen(Color.Gray))
-            {
-                for (int x = 1; x < 11; x++)
-                {
-                    gFiled1P.DrawLine(pen, new Point(x * BlockInfo.BLOCK_WIDTH, 0 * BlockInfo.BLOCK_HEIGHT),
-                        new Point(x * BlockInfo.BLOCK_WIDTH, 20 * BlockInfo.BLOCK_HEIGHT));
-                }
-                for (int y = 1; y < 21; y++)
-                {
-                    gFiled1P.DrawLine(pen, new Point(0 * BlockInfo.BLOCK_WIDTH, y * BlockInfo.BLOCK_HEIGHT),
-                        new Point(20 * BlockInfo.BLOCK_WIDTH, y * BlockInfo.BLOCK_HEIGHT));
-                }
-            }
-
-            //壁と設置されているブロックを描く
-            for (int y = 0; y < GameField.FIELD_HEIGHT; y++)
-            {
-                for (int x = 0; x < GameField.FIELD_WIDTH; x++)
-                {
-                    if (this.BlockField[y, x] >= (int)BlockInfo.BlockType.MINO_IN_FIELD)
-                    {
-                        int y_pos = (this.BlockField[y, x] % (int)BlockInfo.BlockType.MINO_IN_FIELD) * BlockInfo.BLOCK_HEIGHT;
-                        Rectangle srcRect = new Rectangle(0, y_pos, BlockInfo.BLOCK_WIDTH, BlockInfo.BLOCK_HEIGHT);
-                        Rectangle desRect = new Rectangle(0, 0, srcRect.Width, srcRect.Height);
-
-                        desRect.X = (x) * BlockInfo.BLOCK_WIDTH;
-                        desRect.Y = (y) * BlockInfo.BLOCK_HEIGHT;
-                        gFiled1P.DrawImage(BlockSourceImage, desRect, srcRect, GraphicsUnit.Pixel);
-
-                    }
-                }
-            }
-        }
 
         //消去するラインを調べる
         private void CheckEraseLine()
