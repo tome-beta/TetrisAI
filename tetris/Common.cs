99f8d1dedd8c2c537de4e1fa4ce3d35359f13f9d
Susumu-Fukutome <tomex.beta@gmail.com>
2017-09-30 19:00:23 +0900

    追加忘れ

---
+++ tetris/Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetris
+{
+    static class Common
+    {
+        //データ配列
+        public static System.Random MyRandom = new Random();
+    }
+}
